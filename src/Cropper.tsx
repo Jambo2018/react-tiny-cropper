7d68beb9b72cccc7e19864d84c2d045d6e56d7e9
bxtx1992@gmail.com <bxtx1992@gmail.com>
2022-03-18 19:40:41 +0800

    add mouse event

--- src/Cropper.tsx
+++ src/Cropper.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useRef } from "react";
-const Cropper: React.FC = () => {
+interface propsType {
+  shap?: String, // circle , square, rectangle,polygon
+
+
+}
+const Cropper: React.FC<propsType> = (props: propsType) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { shap = "rectangle" } = props;
+
+
   // const onCrop = () => {
   //   const imageElement: any = cropperRef?.current;
   //   const cropper: any = imageElement?.cropper;
   //   // console.log(cropper.getCroppedCanvas().toDataURL());
   // };
   // init canvas and paint the background
-  
+
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas: HTMLCanvasElement = canvasRef.current;
@@ -17,10 +25,29 @@ const Cropper: React.FC = () => {
     ctx.fillRect(0, 0, 20, 60);
   }, [])
 
+  const onMouseDown = (e: any) => {
+    console.log(e)
+  }
+  const onMouseEnter = (e: any) => {
+    console.log(e)
+  }
+  const onMouseMove = (e: any) => {
+    console.log(e)
+  }
+  const onMouseUp = (e: any) => {
+    console.log(e)
+  }
+
+
   return (
     <div style={{ width: "300px", height: "200px", background: "rgba(0,0,0,0.3)" }}>
-      <img src="https://jambo2018.github.io/img/top_img.jpg" width={300} height={200} style={{position:"absolute",zIndex:"-1"}}/>
-      <canvas ref={canvasRef} width={300} height={200} />
+      <img src="https://jambo2018.github.io/img/top_img.jpg" width={300} height={200} style={{ position: "absolute", zIndex: "-1" }} />
+      <canvas ref={canvasRef} width={300} height={200}
+        onMouseDown={onMouseDown}
+        onMouseEnter={onMouseEnter}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      />
     </div>
   );
 };
