6e05e6d1b8fa8f0786f73851d5219c3abcd5772d
bxtx1992 <bxtx1992@gmail.com>
2022-03-19 14:03:42 +0800

    fix merge

--- src/Cropper.tsx
+++ src/Cropper.tsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import Cropper from 'react-easy-crop'
-import { useState,useCallback } from 'react';
-export default function Demo() {
-  const [crop, setCrop] = useState({ x: 0, y: 0 })
-  const [zoom, setZoom] = useState(1)
-
-  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels)
-  }, [])
-
-  return (
-    <Cropper
-      image={"https://raw.githubusercontent.com/Jambo2018/Jambo2018.github.io/main/img/top_img.jpeg"}
-      crop={crop}
-      zoom={zoom}
-      aspect={4 / 3}
-      onCropChange={setCrop}
-      onCropComplete={onCropComplete}
-      onZoomChange={setZoom}
-    />
-  )
-}
-=======
 import React, { useEffect, useRef } from "react";
 interface propsType {
   shap?: String, // circle , square, rectangle,polygon
@@ -81,4 +57,3 @@ const Cropper: React.FC<propsType> = (props: propsType) => {
 };
 
 export default Cropper;
->>>>>>> dev
