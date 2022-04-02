d94ee292fe193455567c42498c0c3583e4756b97
bxtx1992@gmail.com <bxtx1992@gmail.com>
2022-04-02 15:29:01 +0800

    add polygon

--- src/Cropper.tsx
+++ src/Cropper.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import Circle from "./shaps/Circle";
 import Rectangle from "./shaps/Rectangle";
+import Polygon from "./shaps/Polygon";
 
 type Shap = 'circle' | 'square' | 'rectangle' | 'polygon' | never;
 interface propsType {
   type: Shap,
+  nodesNum?: number,
   image: string,
   onResult: (url: string) => void
 }
@@ -15,9 +17,11 @@ const Cropper: React.FC<propsType> = (props: propsType) => {
       case "rectangle":
         return <Rectangle onResult={props.onResult} src={image} />
       case "square":
-        return <Rectangle onResult={props.onResult} src={image} square/>
+        return <Rectangle onResult={props.onResult} src={image} square />
       case "circle":
         return <Circle onResult={props.onResult} src={image} />
+      case "polygon":
+        return <Polygon onResult={props.onResult} dots={props.nodesNum} src={image} />
       default:
         throw new Error("wrong type,the type could only be circle,square,rectangle or polygon");
         break;
