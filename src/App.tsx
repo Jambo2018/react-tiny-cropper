162431a12e240d89eeec954eacc08f4c69bc6ff9
bxtx1992@gmail.com <bxtx1992@gmail.com>
2022-04-07 15:46:08 +0800

    add fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState ,useCallback} from "react";
 import Cropper from "./Cropper";
 import image from  "./assets/test.jpeg";
 import "./App.css";
@@ -8,8 +8,24 @@ function App() {
   const oncrop = (e:string) => {
     setImg(e);
   };
+  // const [count1, setCount1] = useState(0);
+  // const [count2, setCount2] = useState(0);
+  // const [count3, setCount3] = useState(0);
+
+  // const handleClickButton1 = () => {setCount1(count1 + 1)};
+
+  // const handleClickButton2 = useCallback(() => {
+  //   setCount2(count2 + 1);
+  // }, [count2]);
   return (
     <div className="App">
+    {/* <div>
+        <a onClick={handleClickButton1}>Button1{count1}</a>
+        <a onClick={handleClickButton2}>Button2{count2}</a>
+        <a  onClick={() => {setCount3(count3 + 1);     }}>
+          Button3{count3}
+        </a>
+    </div> */}
       <Cropper onResult={oncrop} type="circle" nodesNum={10} image={image}/>
       <img src={img.toString()}/>
     </div>
