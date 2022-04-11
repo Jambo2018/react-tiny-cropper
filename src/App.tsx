2a3a8ee9452cf970fbfee21e6686530278641be6
bxtx1992@gmail.com <bxtx1992@gmail.com>
2022-04-11 17:14:18 +0800

    add image selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState ,useCallback} from "react";
+import { useState, useCallback } from "react";
 import Cropper from "./Cropper";
-import image from  "./assets/test.jpeg";
+// import image from "./assets/test.jpeg";
 import "./App.css";
 
 function App() {
   const [img, setImg] = useState<string>("");
-  const oncrop = (e:string) => {
+  const [image, setImage] = useState<string>("");
+  const oncrop = (e: string) => {
     setImg(e);
   };
   // const [count1, setCount1] = useState(0);
@@ -17,20 +18,32 @@ function App() {
   // const handleClickButton2 = useCallback(() => {
   //   setCount2(count2 + 1);
   // }, [count2]);
+  const onSelectImage = (e: any) => {
+    console.log(e.target.files[0])
+    let reader = new FileReader();
+    reader.onload = function () {
+      let image = "";
+      if (typeof this.result === "string")
+        image = this.result;
+      setImage(image)
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
   return (
     <div className="App">
-    {/* <div>
+      <input type="file" onChange={onSelectImage} />
+      {/* <div>
         <a onClick={handleClickButton1}>Button1{count1}</a>
         <a onClick={handleClickButton2}>Button2{count2}</a>
         <a  onClick={() => {setCount3(count3 + 1);     }}>
           Button3{count3}
         </a>
     </div> */}
-      <Cropper onResult={oncrop} type="square" nodesNum={10} image={image}/>
-      <img src={img.toString()}/>
+      <Cropper onResult={oncrop} type="square" nodesNum={10} image={image} />
+      <img src={img.toString()} />
     </div>
   );
 }
 
 export default App;
- 
