5103d6d4e59efce5ae92c76c16671b63233e34ea
bxtx1992@gmail.com <bxtx1992@gmail.com>
2022-05-05 09:52:32 +0800

    add configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import React,{ useMemo, useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import RecCom from "./shaps/Rectangle";
 import Polygon from "./shaps/Polygon";
 import "./index.css";
 
 type Shap = 'circle' | 'square' | 'rectangle' | 'polygon' | never;
+type ConfigTypes = {
+  backgroundColor?: string,
+  maskColor?: string,
+  cropColor?: string,
+}
 interface propsType {
   type: Shap,
   nodesNum?: number,
   image: string,
   style?: object,
+  configs?:ConfigTypes,
   onResult: (url: string) => void
 }
 const Cropper: React.FC<propsType> = (props: propsType) => {
@@ -19,21 +25,21 @@ const Cropper: React.FC<propsType> = (props: propsType) => {
   const ClipCom = useMemo(() => {
     if (!boxRef.current) return null;
     const { clientWidth, clientHeight } = boxRef.current;
-    const config = { 
-      canvasWidth: clientWidth, 
+    const config = {
+      canvasWidth: clientWidth,
       canvasHeight: clientHeight,
-      src:image
-    }   
-    
+      src: image,
+    }
+
     switch (type) {
       case "rectangle":
         return <RecCom onResult={props.onResult} {...config} />
       case "square":
-        return <RecCom onResult={props.onResult} {...config}  square />
+        return <RecCom onResult={props.onResult} {...config} square />
       case "circle":
-        return <RecCom onResult={props.onResult} {...config}  circle/>
+        return <RecCom onResult={props.onResult} {...config} circle />
       case "polygon":
-        return <Polygon onResult={props.onResult} dots={props.nodesNum || 4} {...config}  />
+        return <Polygon onResult={props.onResult} dots={props.nodesNum || 4} {...config} />
       default:
         throw new Error("wrong type,the type could only be circle,square,rectangle or polygon");
         return;
@@ -42,7 +48,7 @@ const Cropper: React.FC<propsType> = (props: propsType) => {
 
 
   return (
-    <div ref={boxRef} className="box" style={{...props.style, backgroundImage: `url(${image})` }}>
+    <div ref={boxRef} className="box" style={{ ...props.style, backgroundImage: `url(${image})` }}>
       {ClipCom}
     </div>
   );
