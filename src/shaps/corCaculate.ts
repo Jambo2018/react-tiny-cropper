3be20ca7d530bca6d6b04ba429cfca956fd426e5
bxtx1992 <bxtx1992@gmail.com>
2022-05-07 22:20:24 +0800

    DW = 8

--- src/shaps/corCaculate.ts
+++ src/shaps/corCaculate.ts
@@ -2,7 +2,7 @@ import {Last,Cors,Rectangle} from "../type";
 export enum Position { out, in, top_left, top_right, bottom_right, bottom_left, top, right, bottom, left };
 export const rec_curser = ["default", "move", "se-resize", "sw-resize", "se-resize", "ne-resize", "n-resize", "e-resize", "n-resize", "e-resize"]
 // export const square_curser = ["default", "move", "default", "default", "se-resize", "default", "default", "default", "default", "default"]
-export const DW: number = 6;
+export const DW: number = 8;
 // export type Rectangle = {
 //     x: number,
 //     y: number,
